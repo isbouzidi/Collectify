6b05c19932b54a343d810c12d73d65789774de04
bouzidi issam <bouzidissam@gmail.com>
2015-09-21 04:14:24 +0200

    First commit

---
+++ core/config.inc.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Database
+ */
+
+define('DB_TYPE', 'mysql');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'collectify');
+define('DB_USER', 'dbyba');
+define('DB_PASSWORD', 'eagles');
+
+/**
+ * Redbean configuration
+ */
+
+define('REDBEAN_MODEL_PREFIX', '\\Collectify\\Model\\');
